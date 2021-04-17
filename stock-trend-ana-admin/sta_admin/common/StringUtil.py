f2b99c6804fc90ea9c9af44141f7f694dbe3b205
linlejiang <46689621+linlejiang@users.noreply.github.com>
2021-04-16 18:28:28 -0700

    Aaron's backend modifications

--- stock-trend-ana-admin/sta_admin/common/StringUtil.py
+++ stock-trend-ana-admin/sta_admin/common/StringUtil.py
@@ -6,27 +6,30 @@ import os
 from collections import defaultdict
 
 
-def check_is_subset(token, token_file_dir):
+def check_is_subset(token, token_file_dir, flag=False):
     token_files = os.listdir(token_file_dir)
     if token in token_files:
         # if token exist
         return token
     else:
         # if not exist, check is subset
-        t_ticker, t_start, t_end = token.split("_")
-        dir_dict = defaultdict(list)
-        for file in token_files:
-            f_ticker, f_start, f_end = file.split("_")
-            dir_dict[f_ticker].append([f_start, f_end])
+        if flag:
+            t_ticker, t_start, t_end = token.split("_")
+            dir_dict = defaultdict(list)
+            for file in token_files:
+                f_ticker, f_start, f_end = file.split("_")
+                dir_dict[f_ticker].append([f_start, f_end])
 
-        if t_ticker not in dir_dict:
-            return None
+            if t_ticker not in dir_dict:
+                return None
+            else:
+                available_slots = dir_dict[t_ticker]
+                for a_start, a_end in available_slots:
+                    # if [a_start [t_start, t_end] a_end]
+                    if a_start <= t_start and t_end <= a_end:
+                        return t_ticker + '_' + a_start + '_' + a_end
+
+                return None
         else:
-            available_slots = dir_dict[t_ticker]
-            for a_start, a_end in available_slots:
-                # if [a_start [t_start, t_end] a_end]
-                if a_start <= t_start and t_end <= a_end:
-                    return t_ticker + '_' + a_start + '_' + a_end
-
             return None
 
